9db261a4088dbb16bd39a1b9b9ca5525c3d72c34
Pierre Portejoie <portejoiepierre@gmail.com>
2016-11-24 21:50:30 +0100

    back to something more reasonable

--- therealcodeV5.js
+++ therealcodeV5.js
@@ -19,39 +19,14 @@ function generateNoiseMap(seed, width, height) {
                 *(noise.perlin2(x/100 , y/100 )+1)
                 *(noise.perlin2(x/40 , y/40)+1)
                 /2);
-
-            var valueR = Math.abs( (noise.perlin2(x/180 , y/180 )) /2);
-            if (valueR < 0.005) valueR =1; else valueR=0;
-
-            var type = '';
-
-            if (value < 0.3) type = 'sea';
-            else if (value < 0.7) type = 'ground';
-            else type = 'mountain';
-
-            if ( value < 0.1) value = 0.1;
-            else if ( value < 0.2) value = 0.5;
-            else if ( value < 0.3) value = 1;
-            else if ( value < 0.7) value = 0.5;
-            else if ( value < 0.9) value = 0.6;
-            else (value = 1);
+            if (value < 0.1) value = 0.1;
 
 
             value *= 256;
-            valueR *= 256;
             var cell = (x + y * canvas.width) * 4;
-            if (type == 'sea'){
+                data[cell ] = value;
+                data[cell + 1] = value;
                 data[cell + 2] = value;
-            } else if (type == 'ground') {
-                data[cell] = value-valueR;
-                data[cell + 1] = value-valueR;
-                data[cell + 2] = valueR;
-
-            } else {
-                data[cell ] = value-valueR;
-                data[cell + 1] = value-valueR;
-                data[cell + 2] = value-valueR;
-            }
             data[cell] += Math.max(0, (25 - value) * 8);
             data[cell + 3] = 255; // alpha.
         }
@@ -74,5 +49,5 @@ function generateNoiseMap(seed, width, height) {
 
 
 $(document).ready(function() {
-    generateNoiseMap(Math.random(),1000,1000);
+    generateNoiseMap(Math.random(),400 ,400);
 });
